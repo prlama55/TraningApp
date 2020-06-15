e3127a7218e176a2c7b98086bffc43c98db8765e
Padma <prlama@deerhold.org>
2020-06-15 08:29:43 +0545

    React Router

--- react_front/src/App.tsx
+++ react_front/src/App.tsx
@@ -1,18 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-// import Header from "./components/layouts/Header";
-// import Dashboard from "./components/dashboard";
+import Header from "./components/layouts/Header";
+import Dashboard from "./components/dashboard";
+
 // import Cards from "./components/dashboard/Cards";
-import Login from "./components/user/Login";
+ import Login from "./components/user/Login";
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Users from "./components/user/Users";
+import PageNotFound from "./components/PageNotFound";
+import UserRoutes from "./components/user/UserRoutes";
+interface Auth {
+    username: string
+}
 function App() {
+    const initialState: Auth={
+        username: '',
+    }
+    const [auth,setAuth] = useState(initialState)
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        let getAuthFromSession= sessionStorage.getItem("auth");
+        if(getAuthFromSession){
+            const userauth:Auth = JSON.parse(getAuthFromSession.toString())
+            setAuth({
+                username: userauth?userauth.username:'',
+            })
+        }
+    },[]);
+    console.log("auth========",auth)
   return (
-    <div>
-      {/* <Header username="Padma" />
-      <Cards title="Card from APP" />
-      <Dashboard title="dashboard" /> */}
-      <Login />
-    </div>
+      <div>
+          {auth.username &&
+          <BrowserRouter>
+              <Header username="Padma"/>
+              {/*<Route path='/' children={(pro)<Header/>} exact/>*/}
+              <Switch>
+                  <Route path='/' component={Dashboard} exact/>
+                  <Route path='/users' component={UserRoutes}/>
+                  <Route component={PageNotFound}/>
+              </Switch>
+          </BrowserRouter>
+          }
+          {auth.username==='' &&
+          <Login/>
+          }
+      </div>
   );
 }
 
--- react_front/src/components/layouts/Header.tsx
+++ react_front/src/components/layouts/Header.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState } from "react";
+import {NavLink} from 'react-router-dom'
 interface Props {
   username: string;
 }
@@ -43,44 +44,13 @@ const Header: FC<Props> = (props: Props) => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
+              <NavLink  className="nav-link" to='/'>Dashboard</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">
-                  Action
-                </a>
-                <a className="dropdown-item" href="#">
-                  Another action
-                </a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </div>
+              <NavLink className="nav-link" to='/users'>Users</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" href="#">
-                Disabled
-              </a>
+              <NavLink className="nav-link" to='/users/create'>Create User</NavLink>
             </li>
           </ul>
         </div>
